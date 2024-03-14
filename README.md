e38463ad6b2a62edcfea345f498fc1e9074a8bd7
Programmer245 <brunantony24@gmail.com>
2024-03-14 13:53:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Progetto di Ingegneria del Software - A.A. 2023/2024
+
+## Gruppo AM32
+
+Componenti del gruppo:
+
+- Antony Brun
+- Lorenzo Chiroli
+- Jie Chen
+- Matteo Bergagna
+
---
+++ src/main/java/it/polimi/ingsw/am32/HelloController.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.am32;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
