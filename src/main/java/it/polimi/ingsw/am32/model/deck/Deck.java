ec67b622cdc7dfd30c830671a6b368de6cc643b1
Programmer245 <brunantony24@gmail.com>
2024-03-17 22:07:57 +0100

    Added code to Deck class
    Added Javadoc and wrote code for draw and shuffle methods

--- src/main/java/it/polimi/ingsw/am32/model/deck/Deck.java
+++ src/main/java/it/polimi/ingsw/am32/model/deck/Deck.java
@@ -1,18 +1,47 @@
 package it.polimi.ingsw.am32.model.deck;
 
+import it.polimi.ingsw.am32.model.card.Card;
 import java.util.ArrayList;
+import java.util.Collections;
 
+/**
+ * Contains a collection of cards implemented as a stack.
+ */
 public class Deck {
-    private ArrayList<Card> cards;
+    /**
+    * Card at top of deck found at last index of ArrayList.
+    */
+    private final ArrayList<Card> cards;
 
     Deck() {
         cards = new ArrayList<Card>();
+        // TODO Constructor will need to invoke DecksFromDisk class object to load the cards
     }
 
+    /**
+     * Draws (and removes) the top card from the deck.
+     *
+     * @return Card object at the top of the deck if card is available, else null
+     */
     public Card draw() {
-        // TODO
+        if (cards.isEmpty()) return null;
+        else {
+            Card lastCard = cards.getLast();
+            cards.removeLast();
+            return lastCard;
+        }
     }
+
+    /**
+     * Randomly rearranges the cards.
+     *
+     * @return true if deck is not empty, false if deck is empty
+     */
     public boolean shuffle() {
-        // TODO
+        if (cards.isEmpty()) return false;
+        else {
+            Collections.shuffle(cards);
+            return true;
+        }
     }
 }
