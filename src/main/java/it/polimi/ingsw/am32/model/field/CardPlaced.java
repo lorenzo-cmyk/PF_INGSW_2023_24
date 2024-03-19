e70538a7e275f17e54c9378017d3883b5c70ef49
Programmer245 <brunantony24@gmail.com>
2024-03-19 14:29:56 +0100

    Added javadoc and code to class CardPlaced

--- src/main/java/it/polimi/ingsw/am32/model/field/CardPlaced.java
+++ src/main/java/it/polimi/ingsw/am32/model/field/CardPlaced.java
@@ -2,12 +2,25 @@ package it.polimi.ingsw.am32.model.field;
 
 import it.polimi.ingsw.am32.model.card.Card;
 
+/**
+ * Used to store information about a currently placed card. Stores the position of the card in the player's field
+ * along with its orientation.
+ * The coordinates are those of the center of the card.
+ */
 public class CardPlaced {
-    private Card placedCard;
-    private int x;
-    private int y;
-    private boolean isUp;
+    private final Card placedCard;
+    private final int x;
+    private final int y;
+    private final boolean isUp;
 
+    /**
+     * Record the position of the card placed and the face of the card selected by the player.
+     *
+     * @param placedCard the card that player decided to place.
+     * @param x the x coordinate of the card placed.
+     * @param y the y coordinate of the card placed.
+     * @param isUp describe a face of the card that player want to use.
+     */
     public CardPlaced(Card placedCard, int x, int y, boolean isUp) {
         this.placedCard = placedCard;
         this.x = x;
@@ -15,15 +28,38 @@ public class CardPlaced {
         this.isUp = isUp;
     }
 
+    /**
+     * Returns the card.
+     *
+     * @return Card object
+     */
     public Card getCard() {
         return placedCard;
     }
+
+    /**
+     * Returns the x position of the card.
+     *
+     * @return X position of the card in the field.
+     */
     public int getX() {
         return x;
     }
+
+    /**
+     * Returns the y position of the card.
+     *
+     * @return Y position of the card in the field.
+     */
     public int getY() {
         return y;
     }
+
+    /**
+     * Returns card orientation.
+     *
+     * @return true if card is placed front side up, false otherwise
+     */
     public boolean getIsUp() {
         return isUp;
     }
