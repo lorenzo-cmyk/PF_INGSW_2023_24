bf5d847c3188120222c42324309bf929be974152
Programmer245 <brunantony24@gmail.com>
2024-03-19 23:25:20 +0100

    Added code for AnglesCovered strategy

--- src/main/java/it/polimi/ingsw/am32/model/card/pointstrategy/AnglesCovered.java
+++ src/main/java/it/polimi/ingsw/am32/model/card/pointstrategy/AnglesCovered.java
@@ -1,10 +1,27 @@
 package it.polimi.ingsw.am32.model.card.pointstrategy;
 
+import it.polimi.ingsw.am32.model.field.CardPlaced;
 import it.polimi.ingsw.am32.model.field.Field;
 
+/**
+ * Used for cards whose points are calculated on the basis of how many corners the card covers.
+ */
 public class AnglesCovered extends PointStrategy{
+    /**
+     * Calculates the number of angles covered by the given card.
+     *
+     * @param field Field object which the card belongs to
+     * @param x X coordinate of card whose points are being calculated
+     * @param y Y coordinates of card whose points are being calculated
+     * @return Number of angles covered by card
+     */
     int calculateOccurences(Field field, int x, int y) {
-        // TODO
-        return 0;
+        int count = 0;
+        for (CardPlaced i : field.getFieldCards()) { // For each card on the field
+            if (Math.abs(i.getX() - x) == 1 && Math.abs(i.getY() - y) == 1) { // Found card whose corners have been covered by the given card
+                count++;
+            }
+        }
+        return count;
     }
 }
