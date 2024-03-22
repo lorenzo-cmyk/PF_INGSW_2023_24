9cfc572651285451d19c22309e3d29400f6a4cfe
Lorenzo <10797603@polimi.it>
2024-03-23 00:19:51 +0100

    Modified enumeration to avoid relying on entity position

--- src/main/java/it/polimi/ingsw/am32/model/card/pointstrategy/ObjectType.java
+++ src/main/java/it/polimi/ingsw/am32/model/card/pointstrategy/ObjectType.java
@@ -1,11 +1,21 @@
 package it.polimi.ingsw.am32.model.card.pointstrategy;
 
 public enum ObjectType {
-    PLANT,
-    FUNGI,
-    ANIMAL,
-    INSECT,
-    QUILL,
-    INKWELL,
-    MANUSCRIPT
+    PLANT(0),
+    FUNGI(1),
+    ANIMAL(2),
+    INSECT(3),
+    QUILL(4),
+    INKWELL(5),
+    MANUSCRIPT(6);
+
+    private final int value;
+
+    ObjectType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
