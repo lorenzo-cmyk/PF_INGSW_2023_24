44d2b959a9b4bcc1d5f813641bf2220ad1ad3f48
Lorenzo <10797603@polimi.it>
2024-03-22 23:35:19 +0100

    Removed unused attribute validBack and added getters to Card

--- src/main/java/it/polimi/ingsw/am32/model/card/Card.java
+++ src/main/java/it/polimi/ingsw/am32/model/card/Card.java
@@ -5,13 +5,23 @@ import it.polimi.ingsw.am32.model.card.pointstrategy.PointStrategy;
 public class Card {
     private int id;
     private int value;
-    private boolean validBack;
     private PointStrategy pointStrategy;
 
-    public Card(int id, int value, boolean validBack, PointStrategy pointStrategy) {
+    public Card(int id, int value, PointStrategy pointStrategy) {
         this.id = id;
         this.value = value;
-        this.validBack = validBack;
         this.pointStrategy = pointStrategy;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public PointStrategy getPointStrategy() {
+        return pointStrategy;
     }
 }
