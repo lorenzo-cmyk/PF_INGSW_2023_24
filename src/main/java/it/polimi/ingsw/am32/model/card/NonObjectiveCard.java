72bb8c6d4f53a0f8f30aa216d4d21e4b11949c23
Programmer245 <brunantony24@gmail.com>
2024-03-23 21:26:19 +0100

    Added getters in NonObjectiveCard class

--- src/main/java/it/polimi/ingsw/am32/model/card/NonObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/am32/model/card/NonObjectiveCard.java
@@ -3,18 +3,21 @@ package it.polimi.ingsw.am32.model.card;
 import it.polimi.ingsw.am32.model.card.pointstrategy.ObjectType;
 import it.polimi.ingsw.am32.model.card.pointstrategy.PointStrategy;
 
-public class NonObjectiveCard extends Card{
-    private CornerType topLeft;
-    private CornerType topRight;
-    private CornerType bottomLeft;
-    private CornerType bottomRight;
-    private CornerType topLeftBack;
-    private CornerType topRightBack;
-    private CornerType bottomLeftBack;
-    private CornerType bottomRightBack;
-    private int[] permRes;
-    private int[] conditionCount;
-    private ObjectType kingdom;
+/**
+ * Used to represent gold cards, resource cards, and start card.
+ */
+public class NonObjectiveCard extends Card {
+    private final CornerType topLeft;
+    private final CornerType topRight;
+    private final CornerType bottomLeft;
+    private final CornerType bottomRight;
+    private final CornerType topLeftBack;
+    private final CornerType topRightBack;
+    private final CornerType bottomLeftBack;
+    private final CornerType bottomRightBack;
+    private final int[] permRes;
+    private final int[] conditionCount;
+    private final ObjectType kingdom;
 
     public NonObjectiveCard(int id, int value, PointStrategy pointStrategy, CornerType topLeft,
                      CornerType topRight, CornerType bottomLeft, CornerType bottomRight, CornerType topLeftBack,
@@ -35,6 +38,36 @@ public class NonObjectiveCard extends Card{
         // FIXME Need to properly manage permRes, kingdom, and conditionCount initialization
     }
 
+    public CornerType getTopLeft() {
+        return topLeft;
+    }
+    public CornerType getTopRight() {
+        return topRight;
+    }
+    public CornerType getBottomLeft() {
+        return bottomLeft;
+    }
+    public CornerType getBottomRight() {
+        return bottomRight;
+    }
+    public CornerType getTopLeftBack() {
+        return topLeftBack;
+    }
+    public CornerType getTopRightBack() {
+        return topRightBack;
+    }
+    public CornerType getBottomLeftBack() {
+        return bottomLeftBack;
+    }
+    public CornerType getBottomRightBack() {
+        return bottomRightBack;
+    }
+    public int[] getPermRes() {
+        return permRes;
+    }
+    public int[] getConditionCount() {
+        return conditionCount;
+    }
     public ObjectType getKingdom() {
         return kingdom;
     }
