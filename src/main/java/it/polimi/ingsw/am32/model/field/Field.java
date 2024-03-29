27172d8b829c6881bd18171386826800ba671c78
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-03-29 13:13:02 +0100

    Implemented placeCardInField in Field class Added getAllRes (Getter), resourceCornersConverter (Static), checkResRequirements (Static), resourcesObtained (Static) in Field class Fixed availableSpace method in Field class Modified constructor in Field class Added getters in NonObjectiveCard class

--- src/main/java/it/polimi/ingsw/am32/model/field/Field.java
+++ src/main/java/it/polimi/ingsw/am32/model/field/Field.java
@@ -1,11 +1,11 @@
 package it.polimi.ingsw.am32.model.field;
 
+import it.polimi.ingsw.am32.model.card.CornerType;
 import it.polimi.ingsw.am32.model.card.NonObjectiveCard;
-import it.polimi.ingsw.am32.model.card.Card;
 import it.polimi.ingsw.am32.model.card.pointstrategy.ObjectType;
 
 import java.util.ArrayList;
-import java.util.function.BiFunction;
+import java.util.Arrays;
 
 public class Field {
 
@@ -27,29 +27,129 @@ public class Field {
      * @param isUp denote the side of the card chosen by the player
      */
     public Field(NonObjectiveCard initialCard, boolean isUp) {
+
         this.activeRes = new int[resourcesSize];
-        for(int i = 0; i < resourcesSize; i++)
-            activeRes[i] = 0;
         this.fieldCards = new ArrayList<CardPlaced>();
+
         CardPlaced cardPlaced = new CardPlaced(initialCard, 0, 0, isUp);
         fieldCards.addFirst(cardPlaced);
+
+        int[] resToAdd = resourcesObtained(initialCard, isUp);
+
+        activeRes[0] = resToAdd[0];
+        activeRes[1] = resToAdd[1];
+        activeRes[2] = resToAdd[2];
+        activeRes[3] = resToAdd[3];
     }
 
 
     //---------------------------------------------------------------------------------------------
     // Methods
 
-    /**
-     *
-     * @param card
-     * @param x
-     * @param y
-     * @param side
-     * @return
-     */
-    public boolean placeCardInField(NonObjectiveCard card, int x, int y, boolean side) {
-        // TODO
-        return false;
+    // TODO Javadoc
+    public boolean placeCardInField(NonObjectiveCard nonObjectiveCard, int x, int y, boolean side) {
+
+        if( x > 40 || x < -40 || y > 40 || y < -40)
+            return false;
+
+        // Checking resource requirements for placement
+        // TODO parlare modifica CornderType e ObjectType per idea
+
+        if (!checkResRequirements(activeRes, nonObjectiveCard.getConditionCount()))
+            return false;
+
+        // Find possible diagonal cards
+
+        CardPlaced[] tmpCardsPlaced = new CardPlaced[4];
+
+        for (CardPlaced cardPlaced : fieldCards) {
+
+            int tmpX = cardPlaced.getX();
+            int tmpY = cardPlaced.getY();
+
+
+            if (tmpX == x && tmpY == y)
+                return false;
+
+            if (tmpX == x - 1 && tmpY == y + 1)
+                tmpCardsPlaced[0] = cardPlaced;
+            if (tmpX == x + 1 && tmpY == y + 1)
+                tmpCardsPlaced[1] = cardPlaced;
+            if (tmpX == x - 1 && tmpY == y - 1)
+                tmpCardsPlaced[2] = cardPlaced;
+            if (tmpX == x + 1 && tmpY == y - 1)
+                tmpCardsPlaced[3] = cardPlaced;
+        }
+
+        // Check if exist at least one
+        // TODO it might exist a better algorithm
+
+        CornerType[] cornerType = new CornerType[4];
+
+        boolean tmpFilled = false;
+
+        for(int i = 0; i < 4; i++)
+            if (tmpCardsPlaced[i] != null) {
+                tmpFilled = true;
+                break;
+            }
+
+        if(!tmpFilled)
+            return false;
+
+        if (tmpCardsPlaced[0] != null && tmpCardsPlaced[0].getIsUp()) {
+            cornerType[0] = tmpCardsPlaced[0].getNonObjectiveCard().getBottomRight();
+            if (cornerType[0] == CornerType.NON_COVERABLE)
+                return false;
+        }
+        if (tmpCardsPlaced[1] != null && tmpCardsPlaced[1].getIsUp()) {
+            cornerType[1] = tmpCardsPlaced[1].getNonObjectiveCard().getBottomRight();
+            if (cornerType[1] == CornerType.NON_COVERABLE)
+                return false;
+        }
+        if (tmpCardsPlaced[2] != null && tmpCardsPlaced[2].getIsUp()) {
+            cornerType[2] = tmpCardsPlaced[2].getNonObjectiveCard().getBottomRight();
+            if (cornerType[2] == CornerType.NON_COVERABLE)
+                return false;
+        }
+        if (tmpCardsPlaced[3] != null && tmpCardsPlaced[3].getIsUp()) {
+            cornerType[3] = tmpCardsPlaced[3].getNonObjectiveCard().getBottomRight();
+            if (cornerType[3] == CornerType.NON_COVERABLE)
+                return false;
+        }
+
+        // Place card in field
+
+        CardPlaced newCardPlaced = new CardPlaced(nonObjectiveCard, x, y, side);
+
+        fieldCards.addFirst(newCardPlaced);
+
+        // Add gained resources
+        // TODO risorse perse/guadagnate
+
+        int[] resToAdd = resourcesObtained(nonObjectiveCard, side);
+
+        activeRes[0] = activeRes[0] + resToAdd[0];
+        activeRes[1] = activeRes[1] + resToAdd[1];
+        activeRes[2] = activeRes[2] + resToAdd[2];
+        activeRes[3] = activeRes[3] + resToAdd[3];
+        activeRes[4] = activeRes[4] + resToAdd[4];
+        activeRes[5] = activeRes[5] + resToAdd[5];
+        activeRes[6] = activeRes[6] + resToAdd[6];
+
+        // Subtract lost resources
+
+        int[] resToSub = resourceCornersConverter(cornerType[0], cornerType[1], cornerType[2], cornerType[3]);
+
+        activeRes[0] = activeRes[0] - resToSub[0];
+        activeRes[1] = activeRes[1] - resToSub[1];
+        activeRes[2] = activeRes[2] - resToSub[2];
+        activeRes[3] = activeRes[3] - resToSub[3];
+        activeRes[4] = activeRes[4] - resToSub[4];
+        activeRes[5] = activeRes[5] - resToSub[5];
+        activeRes[6] = activeRes[6] - resToSub[6];
+
+        return true;
     }
 
     /**
@@ -82,9 +182,105 @@ public class Field {
         for (CardPlaced fieldCard : fieldCards)
             if (fieldCard.getX() == x && fieldCard.getY() == y)
                 return false;
-        return false;
+        return true;
+
+        //TODO manca controllo sulle carte addiacenti
     }
 
+    /**
+     * Converts the given corners to an array of integers (of size 7) containing the number of occurrences of the 7
+     * resources in the card corners. the array is order following the convention used for ObjectType
+     *
+     * @param first  is the specific corner of the card
+     * @param second is the specific corner of the card
+     * @param third  is the specific corner of the card
+     * @param forth  is the specific corner of the card
+     * @return the array of integer
+     */
+    public static int[] resourceCornersConverter(CornerType first, CornerType second, CornerType third, CornerType forth){
+
+        int[] results = new int[7];
+        CornerType[] tmpCorners = new CornerType[]{first, second, third, forth};
+
+        for(CornerType cornerType: tmpCorners) {
+            if (cornerType == CornerType.PLANT)
+                results[ObjectType.PLANT.getValue()]++;
+            if(cornerType == CornerType.FUNGI)
+                results[ObjectType.FUNGI.getValue()]++;
+            if(cornerType == CornerType.ANIMAL)
+                results[ObjectType.ANIMAL.getValue()]++;
+            if(cornerType == CornerType.INSECT)
+                results[ObjectType.INSECT.getValue()]++;
+            if(cornerType == CornerType.QUILL)
+                results[ObjectType.QUILL.getValue()]++;
+            if(cornerType == CornerType.INKWELL)
+                results[ObjectType.INKWELL.getValue()]++;
+            if(cornerType == CornerType.MANUSCRIPT)
+                results[ObjectType.MANUSCRIPT.getValue()]++;
+        }
+
+        return results;
+    }
+
+    /**
+     * Given the two arrays check if the values of the first array are grater then those of the second array in the
+     * same index for every position of the second array
+     *
+     * @param resources is the array that contain the current resources
+     * @param requirements is the array of the requirements for the resources
+     * @return true if the check result is positive, false if the length of the second array is grater than that of
+     * the first array or if the checking process resulted negatively
+     */
+    public static boolean checkResRequirements(int[] resources, int[] requirements){
+
+        if(requirements.length >= resources.length)
+            return false;
+
+        for (int i = 0; i < requirements.length; i++)
+            if(resources[i] < requirements[i])
+                return false;
+
+        return true;
+    }
+
+    // TODO Javadoc
+    public static int[] resourcesObtained(NonObjectiveCard nonObjectiveCard, boolean isUp){
+
+        int[] result = new int[7];
+
+        if(isUp){
+
+            int[] tmpAdders = resourceCornersConverter(nonObjectiveCard.getTopLeft(), nonObjectiveCard.getTopRight(),
+                    nonObjectiveCard.getBottomLeft(), nonObjectiveCard.getBottomRight());
+
+            result[0] = tmpAdders[0];
+            result[1] = tmpAdders[1];
+            result[2] = tmpAdders[2];
+            result[3] = tmpAdders[3];
+            result[4] = tmpAdders[4];
+            result[5] = tmpAdders[5];
+            result[6] = tmpAdders[6];
+
+
+        }else{
+
+            int[] tmpBases = nonObjectiveCard.getPermRes();
+
+            int[] tmpAdders = resourceCornersConverter(nonObjectiveCard.getTopLeftBack(), nonObjectiveCard.getTopRightBack(),
+                    nonObjectiveCard.getBottomLeftBack(), nonObjectiveCard.getBottomRightBack());
+
+            result[0] = tmpBases[0] + tmpAdders[0];
+            result[1] = tmpBases[1] + tmpAdders[1];
+            result[2] = tmpBases[2] + tmpAdders[2];
+            result[3] = tmpBases[3] + tmpAdders[3];
+            result[4] = tmpBases[4] + tmpAdders[4];
+            result[5] = tmpBases[5] + tmpAdders[5];
+            result[6] = tmpBases[6] + tmpAdders[6];
+
+        }
+
+        return result;
+    }
 
     //---------------------------------------------------------------------------------------------
     // Getters
@@ -106,5 +302,14 @@ public class Field {
      */
     public int getActiveRes(ObjectType type) {
         return activeRes[type.getValue()];
+    }
+
+    /**
+     * Getter
+     *
+     * @return the array containing the amount of each resource of the field
+     */
+    public int[] getAllRes(){
+        return activeRes;
     }
 }
