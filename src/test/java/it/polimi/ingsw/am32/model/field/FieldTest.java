1529734a819995069728113514f045f94ba721a6
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-03-30 16:53:11 +0100

    Created doStructuralTestingOfResourceCornersConverter method in FieldTest class

--- src/test/java/it/polimi/ingsw/am32/model/field/FieldTest.java
+++ src/test/java/it/polimi/ingsw/am32/model/field/FieldTest.java
@@ -1,14 +1,32 @@
 package it.polimi.ingsw.am32.model.field;
 
+import it.polimi.ingsw.am32.model.card.CornerType;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FieldTest {
 
+    /**
+     * Execute an Edge and Condition Structural Test on resourceCornersConverter method
+     */
     @Test
-    void doStructuralTesting(){
+    void doStructuralTestingOfResourceCornersConverter(){
 
+        // Edge and Condition Coverage
 
+        int[] expectedResults = new int[]{1,1,1,1,0,0,0};
+
+        int[] actualResults = Field.resourceCornersConverter(CornerType.PLANT, CornerType.FUNGI,
+                CornerType.ANIMAL, CornerType.INSECT);
+
+        assertArrayEquals(expectedResults, actualResults);
+
+        expectedResults = new int[]{0, 0, 0, 0, 1, 1, 1};
+
+        actualResults = Field.resourceCornersConverter(CornerType.QUILL, CornerType.INKWELL,
+                CornerType.MANUSCRIPT, CornerType.EMPTY);
+
+        assertArrayEquals(expectedResults, actualResults);
     }
 }
