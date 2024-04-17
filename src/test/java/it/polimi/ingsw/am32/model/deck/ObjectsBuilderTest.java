8a937884e0d43b234d379ce2f7fea6709e944bce
Lorenzo <90748278+lorenzo-cmyk@users.noreply.github.com>
2024-04-17 15:58:02 +0200

    Fixed IntellJ warning in ObjectsBuilderTest.java

--- src/test/java/it/polimi/ingsw/am32/model/deck/ObjectsBuilderTest.java
+++ src/test/java/it/polimi/ingsw/am32/model/deck/ObjectsBuilderTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectsBuilderTest {
-    ObjectsBuilder objectsBuilder = new ObjectsBuilder();
+    private final ObjectsBuilder objectsBuilder = new ObjectsBuilder();
 
     @DisplayName("stringToCornerType should return the correct object")
     @Test
