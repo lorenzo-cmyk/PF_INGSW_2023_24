31f6c3224b95e0408645ffefdf952b4f4c70996e
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-04-26 19:55:28 +0200

    Implemented static main, constructor and start methods in Start class

--- src/main/java/it/polimi/ingsw/am32/Server.java
+++ src/main/java/it/polimi/ingsw/am32/Server.java
@@ -1,8 +1,30 @@
 package it.polimi.ingsw.am32;
 
+import it.polimi.ingsw.am32.Utilities.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Server {
-    public static void main(){
-        System.out.println("Server is running");
+
+    private final Logger logger;
+
+    public static void main(String[] args){
+        new Server(args).start();
+    }
+
+    public Server(String[] args) {
+        Configuration.createInstance(args);
+        logger = LogManager.getLogger("Server");
+    }
+
+    public void start() {
+        startSocketServer();
+        startRMIServer();
+        logger.info("Server started");
     }
 
     private void startSocketServer() {
