ec87aa8c9f02ed540799362c79ae955bc79a9b75
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-04-27 23:43:00 +0200

    Added Javadoc to Server class

--- src/main/java/it/polimi/ingsw/am32/Server.java
+++ src/main/java/it/polimi/ingsw/am32/Server.java
@@ -13,16 +13,40 @@ import java.rmi.registry.Registry;
 
 public class Server {
 
+    //---------------------------------------------------------------------------------------------
+    // Variables and Constants
+
     private final Logger logger;
 
+    //---------------------------------------------------------------------------------------------
+    // Static Main
+
+    /**
+     * When the program is started a new {@link Server} object is crated and started.
+     *
+     * @param args usual startup arguments
+     */
     public static void main(String[] args){
         new Server(args).start();
     }
 
+    //---------------------------------------------------------------------------------------------
+    // Constructor
+
+    /**
+     * The {@link Server} class attempt to establish RMI and Socket connection acceptors.
+     * <br>
+     * Furthermore, an instance of {@link Configuration} is created using the extra parameters given in the constructor
+     *
+     * @param args are the parameters to be used for the {@code Configuration} class
+     */
     public Server(String[] args) {
         Configuration.createInstance(args);
         logger = LogManager.getLogger("Server");
     }
+
+    //---------------------------------------------------------------------------------------------
+    // Methods
 
     public void start() {
         startSocketServer();
@@ -30,10 +54,20 @@ public class Server {
         logger.info("Server started");
     }
 
+    /**
+     * This method is used to expose to the outside a socket to accept incoming connections.
+     * <br>
+     * Create an instance of {@link SKClientAcceptor} and submit it to the server {@link java.util.concurrent.ExecutorService}
+     */
     private void startSocketServer() {
         Configuration.getInstance().getExecutorService().submit(new SKClientAcceptor());
     }
 
+    /**
+     * This method is used to expose to the outside an invokable RMI interface.
+     * <br>
+     * Create the RMI {@link Registry} and an instance of {@link RMIClientAcceptor}, finally bind the latter to the former
+     */
     private void startRMIServer() {
         try {
             System.setProperty("java.rmi.server.hostname", Configuration.getInstance().getServerIp());
