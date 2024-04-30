9403816bf39934878442ee209f5798a517d06066
Programmer245 <brunantony24@gmail.com>
2024-04-30 16:38:44 +0200

    Added empty flushMessage method to VirtualView

--- src/main/java/it/polimi/ingsw/am32/controller/VirtualView.java
+++ src/main/java/it/polimi/ingsw/am32/controller/VirtualView.java
@@ -48,4 +48,8 @@ public class VirtualView implements VirtualViewInterface, Runnable {
     public void processMessage(StoCMessage message) {
         // TODO: Process the message
     }
+
+    public void flushMessage() {
+        // TODO
+    }
 }
