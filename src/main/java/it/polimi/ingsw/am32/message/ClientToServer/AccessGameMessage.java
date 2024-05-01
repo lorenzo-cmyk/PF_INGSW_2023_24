5767c8129e63aa4cecf5e0c6997bdd27f90a5542
Programmer245 <brunantony24@gmail.com>
2024-05-01 15:10:57 +0200

    Implemented elaborateMessage method in AccessGameMessage

--- src/main/java/it/polimi/ingsw/am32/message/ClientToServer/AccessGameMessage.java
+++ src/main/java/it/polimi/ingsw/am32/message/ClientToServer/AccessGameMessage.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.am32.message.ClientToServer;
 
 import it.polimi.ingsw.am32.controller.GameController;
 import it.polimi.ingsw.am32.controller.GamesManager;
+import it.polimi.ingsw.am32.controller.exceptions.GameNotFoundException;
+import it.polimi.ingsw.am32.message.ServerToClient.AccessGameFailedMessage;
 import it.polimi.ingsw.am32.network.NodeInterface;
 
 public class AccessGameMessage implements CtoSLobbyMessage {
@@ -15,6 +17,12 @@ public class AccessGameMessage implements CtoSLobbyMessage {
 
     @Override
     public void elaborateMessage(NodeInterface nodeInterface) {
-        // TODO
+        try {
+            GameController gameController = GamesManager.getInstance().accessGame(senderNickname, matchId, nodeInterface);
+            nodeInterface.setGameController(gameController);
+            // Game was successfully joined
+        } catch (GameNotFoundException e) { // Game with given id could not be found; must notify the player trying to join
+            nodeInterface.uploadToClient(new AccessGameFailedMessage(senderNickname, "Game with id " + matchId + " not found"));
+        }
     }
 }
