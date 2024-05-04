1f8d7adea1b94a1ea272b487b1078548f5011129
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-05-04 20:43:23 +0200

    Modified PingTask class (to extend TimerTask)

--- src/main/java/it/polimi/ingsw/am32/network/PingTask.java
+++ src/main/java/it/polimi/ingsw/am32/network/PingTask.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.am32.network;
 
-public class PingTask {
+import java.util.TimerTask;
+
+public class PingTask extends TimerTask {
     private final NodeInterface node;
     public PingTask(NodeInterface node) {
         this.node = node;
