926ca338b9f4542f73efce226e4b6ebab4792e94
JieCver1 <Jie5.chen@mail.polimi.it>
2024-05-05 00:35:54 +0200

    Created UI abstract class that will be extended by GUI and TUI
    Added skeleton of some methods

---
+++ src/main/java/it/polimi/ingsw/am32/client/UI.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.am32.client;
+
+
+import it.polimi.ingsw.am32.Client;
+import it.polimi.ingsw.am32.Server;
+import it.polimi.ingsw.am32.client.listener.AskListener;
+import it.polimi.ingsw.am32.client.listener.ResponseListener;
+import it.polimi.ingsw.am32.network.ClientNodeInterface;
+import it.polimi.ingsw.am32.network.RMIClientAcceptor;
+import it.polimi.ingsw.am32.network.RMIClientNode;
+import it.polimi.ingsw.am32.network.SKClientNode;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public abstract class UI implements View, EventHandler{
+    ClientNodeInterface clientNode;
+    public UI() {
+        //TODO
+    }
+
+    public abstract void InitializeViewElement();
+
+    public abstract void showWelcome();
+
+    public abstract void askSelectGameMode();
+
+    public abstract void askNickname();
+
+    public abstract void askCreateGame();
+
+    public abstract void handleEvent(Event event, int Choice);
+
+    public abstract void askJoinGame();
+
+    public abstract void askReconnectGame();
+
+    public abstract void askPlaceCard();
+    public void setSocketClient(String ServerIP, int port) throws IOException {
+        SKClientNode clientNode = new SKClientNode(this);
+        this.clientNode = clientNode;
+        clientNode.startConnection(ServerIP,port);
+        //TODO verify if this is correct
+    }
+    public void setRMIClient(String ServerURL){
+        //TODO verify if this is correct
+        this.clientNode = new RMIClientNode(this);
+        try{
+            Registry registry = LocateRegistry.getRegistry(ServerURL);
+            String remoteObjectName = "Server-CodexNaturalis";
+            RMIClientAcceptor rmiClientAcceptor = (RMIClientAcceptor) registry.lookup(remoteObjectName);
+            this.clientNode = new RMIClientNode(this);
+            System.out.println("RMI Client Acceptor created");
+        }catch (RemoteException | NotBoundException e) {
+            //TODO
+        }
+    }
+}
+
+
