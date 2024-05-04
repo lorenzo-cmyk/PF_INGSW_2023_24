ccfce3b770f46d7de5563ea9f3a6338012ced2f9
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-05-04 22:51:18 +0200

    Updated NodeInterface interface

--- src/main/java/it/polimi/ingsw/am32/network/NodeInterface.java
+++ src/main/java/it/polimi/ingsw/am32/network/NodeInterface.java
@@ -1,9 +1,10 @@
 package it.polimi.ingsw.am32.network;
 
 import it.polimi.ingsw.am32.message.ServerToClient.StoCMessage;
+import it.polimi.ingsw.am32.network.exceptions.UploadFailureException;
 
 public interface NodeInterface {
-    void uploadToClient(StoCMessage message);
+    void uploadToClient(StoCMessage message) throws UploadFailureException;
 
     void pingTimeOverdue();
 
