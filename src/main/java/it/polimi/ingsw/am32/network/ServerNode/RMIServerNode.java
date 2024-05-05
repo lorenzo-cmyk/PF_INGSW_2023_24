1b00774ac82cefa310ac94aa2424cac7890904bd
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-05-05 17:43:35 +0200

    Updated class scheme in RMIServerNode class Implemented setGameController method in RMIServerNode class

--- src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNode.java
+++ src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNode.java
@@ -3,16 +3,23 @@ package it.polimi.ingsw.am32.network.ServerNode;
 import it.polimi.ingsw.am32.controller.GameController;
 import it.polimi.ingsw.am32.message.ClientToServer.CtoSMessage;
 import it.polimi.ingsw.am32.message.ServerToClient.StoCMessage;
+import it.polimi.ingsw.am32.network.ClientNode.RMIClientNodeInt;
 
 public class RMIServerNode implements RMIServerNodeInt, NodeInterface {
+
     private GameController gameController;
     private int pingCount;
     public RMIServerNode(GameController gameController) {
         this.gameController = gameController;
+    private RMIClientNodeInt clientNode;
+
+    public RMIServerNode(RMIClientNodeInt clientNode) {
     }
+
     public void uploadCtoS(CtoSMessage message) {
         //TODO
     }
+
     public void uploadToClient(StoCMessage message) {
         //TODO
     }
@@ -20,8 +27,13 @@ public class RMIServerNode implements RMIServerNodeInt, NodeInterface {
     public void pingTimeOverdue() {
         //TODO
     }
+
     public void resetTimeCounter(){
         //TODO
+    }
+
+    public void setGameController(GameController gameController) {
+        this.gameController = gameController;
     }
 
 }
