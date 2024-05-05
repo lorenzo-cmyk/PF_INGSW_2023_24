d6e4202661228ba0f02b4f6ad48950adbe197177
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-05-05 18:26:33 +0200

    Updated class scheme in RMIClientNodeInt class

--- src/main/java/it/polimi/ingsw/am32/network/ClientNode/RMIClientNodeInt.java
+++ src/main/java/it/polimi/ingsw/am32/network/ClientNode/RMIClientNodeInt.java
@@ -1,6 +1,11 @@
 package it.polimi.ingsw.am32.network.ClientNode;
 
+import it.polimi.ingsw.am32.message.ServerToClient.StoCMessage;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface RMIClientNodeInt extends Remote {
+
+    void uploadStoC(StoCMessage message) throws RemoteException;
 }
