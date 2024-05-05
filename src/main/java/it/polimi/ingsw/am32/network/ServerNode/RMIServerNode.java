e9966a421ca9c9c6204fe84505f33a1d51ea3f78
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-05-05 18:29:16 +0200

    Added attributes to RMIServerNode class

--- src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNode.java
+++ src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNode.java
@@ -1,18 +1,25 @@
 package it.polimi.ingsw.am32.network.ServerNode;
 
+import it.polimi.ingsw.am32.Utilities.Configuration;
 import it.polimi.ingsw.am32.controller.GameController;
 import it.polimi.ingsw.am32.message.ClientToServer.CtoSMessage;
 import it.polimi.ingsw.am32.message.ServerToClient.StoCMessage;
 import it.polimi.ingsw.am32.network.ClientNode.RMIClientNodeInt;
+import it.polimi.ingsw.am32.network.exceptions.UploadFailureException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class RMIServerNode extends UnicastRemoteObject implements RMIServerNodeInt, NodeInterface {
 
+    private final Logger logger;
+    private final Configuration config;
     private GameController gameController;
     private int pingCount;
     private RMIClientNodeInt clientNode;
+    private PingTask pingTask;
 
     public RMIServerNode(RMIClientNodeInt clientNode) {
     public RMIServerNode(RMIClientNodeInt clientNode) throws RemoteException {
@@ -30,7 +37,7 @@ public class RMIServerNode extends UnicastRemoteObject implements RMIServerNodeI
         //TODO
     }
 
-    public void resetTimeCounter(){
+    public void resetTimeCounter() {
         //TODO
     }
 
