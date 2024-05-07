360264be116c59810c2d234c7041547aed872103
JieCver1 <Jie5.chen@mail.polimi.it>
2024-05-07 23:38:04 +0200

    Added and implemented CardFactory to create cards in side of the client
    In this way, based on the descriptions in JSON file, we could print the cards in CLI

---
+++ src/main/java/it/polimi/ingsw/am32/client/exceptions/MissingJSONException.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.am32.client.exceptions;
+
+/**
+ * This class represents a custom exception that is thrown when a required JSON file is missing.
+ * It extends the RuntimeException class, meaning it is an unchecked exception.
+ * Unchecked exceptions do not need to be declared in a method or constructor's throws clause.
+ */
+public class MissingJSONException extends RuntimeException {
+    /**
+     * Constructs a new MissingJSONException with the specified detail message.
+     * The detail message is saved for later retrieval by the Throwable.getMessage() method.
+     *
+     * @param message the detail message. The detail message is saved for later retrieval by the
+     *                Throwable.getMessage() method.
+     */
+    public MissingJSONException(String message) {
+        super(message);
+    }
+}
