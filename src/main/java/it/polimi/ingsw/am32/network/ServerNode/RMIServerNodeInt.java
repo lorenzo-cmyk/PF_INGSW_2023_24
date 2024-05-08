ccf2b8888969d0f6fb48f4e4be928ac687abcb17
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-05-08 15:03:41 +0200

    Modified class scheme in RMIServerNodeInt class

--- src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNodeInt.java
+++ src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNodeInt.java
@@ -1,10 +1,11 @@
 package it.polimi.ingsw.am32.network.ServerNode;
 
 import it.polimi.ingsw.am32.message.ClientToServer.CtoSMessage;
+import it.polimi.ingsw.am32.model.exceptions.PlayerNotFoundException;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RMIServerNodeInt extends Remote {
-    void uploadCtoS(CtoSMessage message) throws RemoteException;
+    void uploadCtoS(CtoSMessage message) throws RemoteException, PlayerNotFoundException;
 }
