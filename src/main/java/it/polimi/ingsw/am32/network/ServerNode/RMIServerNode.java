ff93c31d5cd8aeeefbdf855383dd73c70e6529fc
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-05-08 15:10:21 +0200

    Implemented prototype of RMIServerNode and SKServerNode

--- src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNode.java
+++ src/main/java/it/polimi/ingsw/am32/network/ServerNode/RMIServerNode.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.am32.Utilities.Configuration;
 import it.polimi.ingsw.am32.controller.GameController;
 import it.polimi.ingsw.am32.message.ClientToServer.CtoSMessage;
 import it.polimi.ingsw.am32.message.ServerToClient.StoCMessage;
+import it.polimi.ingsw.am32.model.exceptions.PlayerNotFoundException;
 import it.polimi.ingsw.am32.network.ClientNode.RMIClientNodeInt;
 import it.polimi.ingsw.am32.network.exceptions.UploadFailureException;
 import org.apache.logging.log4j.LogManager;
@@ -19,29 +20,80 @@ public class RMIServerNode extends UnicastRemoteObject implements RMIServerNodeI
     private GameController gameController;
     private int pingCount;
     private RMIClientNodeInt clientNode;
-    private PingTask pingTask;
+    private final PingTask pingTask;
+    private boolean pingTaskAvailable;
 
     public RMIServerNode(RMIClientNodeInt clientNode) throws RemoteException {
+        this.clientNode = clientNode;
+        pingTask = new PingTask(this);
+        config = Configuration.getInstance();
+        pingCount = config.getMaxPingCount();
+        pingTaskAvailable = true;
+
+        logger = LogManager.getLogger("RMIServerNode");
     }
 
     public void uploadCtoS(CtoSMessage message) throws RemoteException {
-        //TODO
+
+        resetTimeCounter();
+        message.elaborateMessage(gameController);
     }
 
-    public void uploadToClient(StoCMessage message) {
-        //TODO
+    public void uploadToClient(StoCMessage message) throws UploadFailureException {
+
+        try {
+            clientNode.uploadStoC(message);
+            logger.info("StoCMessage sent to client");
+
+        } catch (RemoteException e) {
+            logger.error("Failed to send StoCMessage to client: {}",  e.getMessage());
+            throw new UploadFailureException();
+        }
     }
 
     public void pingTimeOverdue() {
-        //TODO
+        synchronized (pingTask) {
+            if(!pingTaskAvailable)
+                return;
+
+            pingCount--;
+
+            if(pingCount == 0){
+                //TODO il metodo seguente è ancora questo??
+
+                destroy();
+            }
+        }
     }
 
     public void resetTimeCounter() {
-        //TODO
+        synchronized (pingTask){
+            if(!pingTaskAvailable)
+                return;
+
+            pingCount = config.getMaxPingCount();
+        }
+    }
+
+    public void destroy() {
+
+        synchronized (pingTask){
+            if(!pingTaskAvailable)
+                return;
+
+            pingTask.cancel();
+            pingTaskAvailable = false;
+        }
+
+        // TODO eliminare pingTask da timer in controller
+
+
+        //UnicastRemoteObject.unexportObject(this, false);
     }
 
     public void setGameController(GameController gameController) {
         this.gameController = gameController;
+        // Aggiungi PingTask al controller
     }
 
 }
