d8851151a9beeb64d21ac88a7d3c9c636c45e2c7
JieCver1 <Jie5.chen@mail.polimi.it>
2024-05-11 14:23:51 +0200

    Modified the PlayerPub class adding the setters
    and deleting matrix 2d board

--- src/main/java/it/polimi/ingsw/am32/client/PlayerPub.java
+++ src/main/java/it/polimi/ingsw/am32/client/PlayerPub.java
@@ -1,31 +1,22 @@
 package it.polimi.ingsw.am32.client;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Use this class to store the player's public information that is shared with other players, which means that in this
  * class contains all the information that the player can request to know about the other players.
  */
 public class PlayerPub {
-    private final String nickname;
     private final String colour;
-    private final int points;
+    private int points;
     private final ArrayList<CardPlacedView> field;
-    private final String [][] board; //TODO: not sure if this is useful also for GUI
-    private final int[]resources;
+    private int[]resources;
 
-    public PlayerPub(String nickname, String colour, int points, String[][] board,ArrayList<CardPlacedView> field,int[]resources) {
-        this.nickname = nickname;
+    public PlayerPub( String colour, int points,ArrayList<CardPlacedView> field,int[]resources) {
         this.colour = colour;
         this.points = points;
-        this.board = board;
         this.field = field;
         this.resources = resources;
-    }
-    public String getNickname() {
-
-        return nickname;
     }
     public String getColour() {
 
@@ -35,14 +26,32 @@ public class PlayerPub {
 
         return points;
     }
-    public String[][] getBoard() {
-        return board;
-    }
     public ArrayList<CardPlacedView> getField() {
 
         return field;
     }
     public int[] getResources(){
         return resources;
+    }
+
+    /**
+     * update the resources of the player with the new resources received from the message.
+     * @param resources the new version of the resources.
+     */
+    public void updateResources(int[] resources){
+        this.resources = resources.clone();
+    }
+    /**
+     * Update the points of the player with the points updated received from the message.
+     */
+    public void updatePoints(int points){
+        this.points = points;
+    }
+    /**
+     * Add the card placed on the field to the field of the player.
+     * @param card the card placed on the field.
+     */
+    public void addToField(CardPlacedView card){
+        this.field.add(card);
     }
 }
