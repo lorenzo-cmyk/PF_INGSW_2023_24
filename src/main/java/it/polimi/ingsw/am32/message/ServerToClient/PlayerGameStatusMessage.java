5e499a087021c8e204f2fe070fc47fb146be27cc
Programmer245 <brunantony24@gmail.com>
2024-05-13 23:06:44 +0200

    Added javadoc to fields of PlayerGameStatusMessage class

--- src/main/java/it/polimi/ingsw/am32/message/ServerToClient/PlayerGameStatusMessage.java
+++ src/main/java/it/polimi/ingsw/am32/message/ServerToClient/PlayerGameStatusMessage.java
@@ -6,23 +6,77 @@ import it.polimi.ingsw.am32.client.View;
 import java.util.ArrayList;
 
 public class PlayerGameStatusMessage implements StoCMessage {
+    /**
+     * Nickname of the recipient of the message
+     */
     private final String recipientNickname;
+    /**
+     * List of nicknames of the players in the game (connected or disconnected)
+     */
     private final ArrayList<String> playerNicknames;
+    /**
+     * List of booleans that represent the connection status of the players in the game
+     */
     private final ArrayList<Boolean> playerConnected;
+    /**
+     * List of integers that represent the colours of the players in the game
+     */
     private final ArrayList<Integer> playerColours;
+    /**
+     * List of integers that represent the ids of the cards in the hand of the player
+     */
     private final ArrayList<Integer> playerHand;
+    /**
+     * Integer that represents the id of the chosen secret objective of the player
+     */
     private final int playerSecretObjective;
+    /**
+     * Array of integers that represent the points held by each player
+     */
     private final int[] playerPoints;
+    /**
+     * List of lists of arrays of integers that represent the fields of each player
+     */
     private final ArrayList<ArrayList<int[]>> playerFields;
+    /**
+     * Array of integers that represent the resources of the player
+     */
     private final int[] playerResources;
+    /**
+     * List of integers that represent the ids of the common objectives of the game
+     */
     private final ArrayList<Integer> gameCommonObjectives;
+    /**
+     * List of integers that represent the ids of the place-up resource cards in the game
+     */
     private final ArrayList<Integer> gameCurrentResourceCards;
+    /**
+     * List of integers that represent the ids of the place-up gold cards in the game
+     */
     private final ArrayList<Integer> gameCurrentGoldCards;
+    /**
+     * Integer that represents the size of the resources deck
+     */
     private final int gameResourcesDeckSize;
+    /**
+     * Integer that represents the size of the gold deck
+     */
     private final int gameGoldDeckSize;
+    /**
+     * Integer that represents the status of the match
+     */
     private final int matchStatus;
+    /**
+     * List of chat messages that represent the chat history of the game
+     */
     private final ArrayList<ChatMessage> chatHistory;
+    /**
+     * String that represents the nickname of the current player
+     */
     private final String currentPlayer;
+    /**
+     * List of arrays of integers that represent the available field spaces for the player to place a card
+     */
     private final ArrayList<int[]> newAvailableFieldSpaces;
 
     public PlayerGameStatusMessage(String recipientNickname, ArrayList<String> playerNicknames, ArrayList<Boolean> playerConnected, ArrayList<Integer> playerColours,
