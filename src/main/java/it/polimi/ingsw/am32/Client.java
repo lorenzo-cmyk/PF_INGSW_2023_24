a3b49d732c4bcdfd8ce2215b9bc9b8f8566490a7
Programmer245 <brunantony24@gmail.com>
2024-05-19 19:51:35 +0200

    Removed recursive behaviour of chooseUI method in Client class

--- src/main/java/it/polimi/ingsw/am32/Client.java
+++ src/main/java/it/polimi/ingsw/am32/Client.java
@@ -6,14 +6,29 @@ import it.polimi.ingsw.am32.client.view.tui.TextUI;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
-
 import java.io.PrintStream;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * The main class on the client side of the application.
+ * Prompts the user to choose whether to play the game in GUI or TUI mode.
+ * After the user has chosen, calls the appropriate view object.
+ *
+ * @author Jie
+ */
 public class Client {
+    /**
+     * Used to read the user's input
+     */
     private static final Scanner in = new Scanner(System.in);
+    /**
+     * Used to display output text on the user's terminal
+     */
     private static final PrintStream out= new PrintStream(System.out);
+    /**
+     * Used for logging purposes
+     */
     private static final Logger logger = LogManager.getLogger("ClientLogger");
 
     public static void main(String[] args){
@@ -22,39 +37,45 @@ public class Client {
         chooseUI();
     }
 
+    /**
+     * Prompts the user to choose whether to play the game in GUI or TUI mode
+     */
     public static void chooseUI(){
-        View view = null;
+        View view;
+
         out.println("""
                 Choose the User Interface:
                 1. Text User Interface
                 2. Graphical User Interface""");
-        int uiChoice;
-        while (true) {
+
+        int uiChoice; // Stores an integer indicating the user's choice (GUI or TUI)
+        while (true) { // Keep looping until the user enters a valid choice
             try {
-                uiChoice = in.nextInt();
-                break;
-            } catch (InputMismatchException e) {
+                uiChoice = in.nextInt(); // Read the user's input
+            } catch (InputMismatchException e) { // User entered non-integer
                 out.println("! Invalid input, please try again");
-                in.nextLine();
+                continue; // Continue the loop
             }
+
+            switch (uiChoice) { // Check the user's choice
+                case 1: {
+                    view = new TextUI();
+                    break;
+                }
+                case 2: {
+                    view = new GraphicalUI();
+                    break;
+                }
+                default: {
+                    out.println("Invalid choice, please try again");
+                    continue; // Continue the loop
+                }
+            }
+
+            // If we get to this point, the user has entered a valid choice
+            break;
         }
-        switch (uiChoice) {
-            case 1: {
-                view = new TextUI();
-                break;
-            }
-            case 2: {
-                view = new GraphicalUI();
-                break;
-            }
-            default: {
-                out.println("Invalid choice, please try again");
-                chooseUI();
-                break;
-            }
-        }
-        if (view != null) {
-            view.launch();
-        }
+
+        view.launch(); // Start the game using the chosen UI
     }
 }
