131d2a4aefe122296fdc8ac25bcebd6ac4ce564d
JieCver1 <Jie5.chen@mail.polimi.it>
2024-06-12 23:05:14 +0200

    Added Javadoc in PlayerPub

--- src/main/java/it/polimi/ingsw/am32/client/PlayerPub.java
+++ src/main/java/it/polimi/ingsw/am32/client/PlayerPub.java
@@ -7,12 +7,35 @@ import java.util.ArrayList;
  * class contains all the information that the player can request to know about the other players.
  */
 public class PlayerPub {
+    /**
+     * The colour of the player.
+     */
     private String colour;
+    /**
+     * The points of the player.
+     */
     private int points;
+    /**
+     * The field of the player that contains the information of the cards placed on the field.
+     */
     private final ArrayList<CardPlacedView> field;
+    /**
+     * The resources of the player in the field with order: PLANT, FUNGI, ANIMAL, INSECT, QUILL, INKWELL, MANUSCRIPT.
+     */
     private int[]resources;
+    /**
+     * The boolean value that indicates if the player is online or offline.
+     */
     private boolean isOnline;
 
+    /**
+     * The constructor of the class that initializes the player's public information with the given parameters.
+     * @param colour The colour of the player.
+     * @param points The points of the player.
+     * @param field The field of the player that contains the information of the cards placed on the field.
+     * @param resources The resources of the player in the field.
+     * @param isOnline The boolean value that indicates if the player is online or offline.
+     */
     public PlayerPub( String colour, int points,ArrayList<CardPlacedView> field,int[]resources,boolean isOnline){
         this.colour = colour;
         this.points = points;
@@ -20,22 +43,38 @@ public class PlayerPub {
         this.resources = resources;
         this.isOnline = isOnline;
     }
-    public String getColour() {
 
+    /**
+     * The getter method for the colour of the player.
+     * @return The colour of the player.
+     */
+    public String getColour() {
         return colour;
     }
-    public int getPoints() {
 
+    /**
+     * The getter method for the points of the player.
+     * @return The points of the player.
+     */
+    public int getPoints() {
         return points;
     }
-    public ArrayList<CardPlacedView> getField() {
 
+    /**
+     * The getter method for the field of the player.
+     * @return The field of the player.
+     */
+    public ArrayList<CardPlacedView> getField() {
         return field;
     }
+
+    /**
+     * The getter method for the resources of the player.
+     * @return The array of integers that stores the resources of the player in the field.
+     */
     public int[] getResources(){
         return resources;
     }
-
     /**
      * update the resources of the player with the new resources received from the message.
      * @param resources the new version of the resources.
@@ -56,9 +95,19 @@ public class PlayerPub {
     public void addToField(CardPlacedView card){
         this.field.add(card);
     }
+
+    /**
+     * The setter method for the colour of the player to update the colour of the player.
+     * @param colour the new colour of the player.
+     */
     public void updateColour(String colour){
         this.colour = colour;
     }
+
+    /**
+     * The setter method for the boolean value that indicates if the player is online or offline.
+     * @param isOnline the current status of the player.
+     */
     public void updateOnline(boolean isOnline){
         this.isOnline = isOnline;
     }
