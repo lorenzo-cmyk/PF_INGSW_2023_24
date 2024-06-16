2e11058bd52529143a0eecde72f8c5d339534434
Lorenzo <10797603@polimi.it>
2024-06-16 17:44:28 +0200

    Added errorType in ErrorMessage.java

--- src/main/java/it/polimi/ingsw/am32/message/ServerToClient/ErrorMessage.java
+++ src/main/java/it/polimi/ingsw/am32/message/ServerToClient/ErrorMessage.java
@@ -5,15 +5,17 @@ import it.polimi.ingsw.am32.client.View;
 public class ErrorMessage implements StoCMessage{
     private final String message;
     private final String recipientNickname;
+    private final int errorType;
 
-    public ErrorMessage(String message, String recipientNickname) {
+    public ErrorMessage(String message, String recipientNickname, int errorType) {
         this.message = message;
         this.recipientNickname = recipientNickname;
+        this.errorType = errorType;
     }
 
     @Override
     public void processMessage(View view) {
-        view.handleFailureCase(view.getEvent(),message);
+        view.handleFailureCase(view.getEvent(), message);
     }
 
     @Override
@@ -26,6 +28,7 @@ public class ErrorMessage implements StoCMessage{
         return "ErrorMessage:{" +
                 "message='" + message + '\'' +
                 ", recipientNickname='" + recipientNickname + '\'' +
+                ", errorType=" + errorType +
                 '}';
     }
 }
