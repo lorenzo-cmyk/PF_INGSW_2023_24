1e5a97afdb332337d935423273ce8e022e5014ab
JieCver1 <Jie5.chen@mail.polimi.it>
2024-06-22 22:58:11 +0200

    Added Javadoc for a part of ServerToClient messages

--- src/main/java/it/polimi/ingsw/am32/message/ServerToClient/AssignedStarterCardMessage.java
+++ src/main/java/it/polimi/ingsw/am32/message/ServerToClient/AssignedStarterCardMessage.java
@@ -3,21 +3,47 @@ package it.polimi.ingsw.am32.message.ServerToClient;
 import it.polimi.ingsw.am32.client.Event;
 import it.polimi.ingsw.am32.client.View;
 
+/**
+ * This class is used to manage the message sent by the server to the client to assign the starter card to the player.
+ * The player will receive the starter card and will be able to select the side of the card.
+ */
+
 public class AssignedStarterCardMessage implements StoCMessage {
+    /**
+     * The nickname of the recipient who will receive the assignment of the starter card.
+     */
     private final String recipientNickname;
+    /**
+     * The id of the starter card assigned to the player.
+     */
     private final int cardId;
 
+    /**
+     * The constructor of the class: it creates a new AssignedStarterCardMessage with the nickname of the recipient who
+     * will receive the assignment of the starter card and the id of the card assigned to the player.
+     * @param recipientNickname the nickname of the player who will receive the message.
+     * @param cardId the id of the starter card assigned to the player.
+     */
     public AssignedStarterCardMessage(String recipientNickname, int cardId) {
         this.recipientNickname = recipientNickname;
         this.cardId = cardId;
     }
 
+    /**
+     * This method is used to process the message to the client when the starter card is assigned to the player,
+     * setting the starter card received by the server and updating the current event of the game.
+     * @param view the view of the player who will receive the message and should be updated.
+     */
     @Override
     public void processMessage(View view) {
         view.setStarterCard(cardId); // store the starter card
         view.updateCurrentEvent(Event.SELECT_STARTER_CARD_SIDE);// set the current event
     }
 
+    /**
+     * This method is used to get the nickname of the recipient who will receive the assignment of the starter card.
+     * @return the nickname of the player who will receive the message.
+     */
     @Override
     public String getRecipientNickname() {
         return recipientNickname;
--- src/main/java/it/polimi/ingsw/am32/message/ServerToClient/DeckSizeUpdateMessage.java
+++ src/main/java/it/polimi/ingsw/am32/message/ServerToClient/DeckSizeUpdateMessage.java
@@ -4,15 +4,51 @@ import it.polimi.ingsw.am32.client.View;
 
 import java.util.Arrays;
 
+/**
+ * This class is used to manage the message sent by the server to the client to update the deck after one player
+ * draws a card.
+ */
 public class DeckSizeUpdateMessage implements StoCMessage {
+    /**
+     * The nickname of the recipient who will receive the message to update the deck size after the draw card action.
+     */
     private final String recipientNickname;
+    /**
+     * The size of the resource card deck after the draw card action.
+     */
     private final int resourceCardDeckSize;
+    /**
+     * The resource card deck facing kingdom after the draw card action.
+     */
     private final int resourceCardDeckFacingKingdom;
+    /**
+     * The size of the gold card deck after the draw card action.
+     */
     private final int goldCardDeckSize;
+    /**
+     * The gold card deck facing kingdom after the draw card action.
+     */
     private final int goldCardDeckFacingKingdom;
+    /**
+     * The current resource cards in the deck after the draw card action.
+     */
     private final int[] currentResourceCards;
+    /**
+     * The current gold cards in the deck after the draw card action.
+     */
     private final int[] currentGoldCards;
 
+    /**
+     * The constructor of the class: it creates a new DeckSizeUpdateMessage with the nickname of the recipient who will
+     * receive the message to update the deck after the draw card action.
+     * @param recipientNickname the nickname of the player who will receive the message.
+     * @param resourceCardDeckSize the size of the resource card deck after the draw card action.
+     * @param goldCardDeckSize the size of the gold card deck after the draw card action.
+     * @param currentResourceCards the current resource card visible after the draw card action.
+     * @param currentGoldCards the current gold card visible after the draw card action.
+     * @param resourceCardDeckFacingKingdom the resource card deck facing kingdom after the draw card action.
+     * @param goldCardDeckFacingKingdom the gold card deck facing kingdom after the draw card action.
+     */
     public DeckSizeUpdateMessage(String recipientNickname, int resourceCardDeckSize, int goldCardDeckSize,
                                  int[] currentResourceCards, int[] currentGoldCards,
                                  int resourceCardDeckFacingKingdom, int goldCardDeckFacingKingdom) {
@@ -25,11 +61,20 @@ public class DeckSizeUpdateMessage implements StoCMessage {
         this.goldCardDeckFacingKingdom = goldCardDeckFacingKingdom;
     }
 
+    /**
+     * This method is used to process the message to the client when the deck is updated after the draw card action.
+     * It updates the view with the new deck size and the current cards in the deck.
+     * @param view the view of the player who will receive the message and should be updated.
+     */
     @Override
     public void processMessage(View view) {
         view.updateDeck(resourceCardDeckSize, goldCardDeckSize, currentResourceCards, currentGoldCards,resourceCardDeckFacingKingdom ,goldCardDeckFacingKingdom );
     }
 
+    /**
+     * This method is used to get the nickname of the recipient who will receive the message to update the deck.
+     * @return the nickname of the player who will receive the message.
+     */
     @Override
     public String getRecipientNickname() {
         return recipientNickname;
