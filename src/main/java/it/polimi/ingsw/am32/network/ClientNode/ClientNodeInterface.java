8412764a9f96d1b950ebb85ebbc15ce6f2c7c485
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-06-26 17:17:08 +0200

    Added Javadoc to ServerNodeInterface class Modified Javadoc in ClientNodeInterface class

--- src/main/java/it/polimi/ingsw/am32/network/ClientNode/ClientNodeInterface.java
+++ src/main/java/it/polimi/ingsw/am32/network/ClientNode/ClientNodeInterface.java
@@ -7,12 +7,15 @@ import it.polimi.ingsw.am32.network.exceptions.UploadFailureException;
 /**
  * This {@code ClientNodeInterface} interface is used to identify all ClientNodes through which communication with the
  * server can happen. <br>
- * It contains all the methods that another class in the client can invoke on any ClientNode.
+ * It's objective is to mask behind the interface the complexity of managing the network part of the client, so that the
+ * other classes of the client don't have to know how is the communication between server and client working. <br>
+ * It contains all the methods that another class in the client can invoke on any ClientNode. <br>
+ * It's important to know that it's necessary and also enough to create one instance of ClientNode per client for the
+ * entire duration of the game.
  *
  * @author Matteo
  */
 public interface ClientNodeInterface {
-
 
     //---------------------------------------------------------------------------------------------
     // Methods
@@ -40,9 +43,9 @@ public interface ClientNodeInterface {
     void uploadToServer(CtoSLobbyMessage message) throws UploadFailureException;
 
     /**
-     * Inform the ClientNode that the interval for pongs is over.
-     * This invocation can lead to the decrease of a pong counter and if needed to the reset of the connection of the
-     * ClientNode.
+     * Inform the ClientNode that the interval for pongs is over. <br>
+     * This invocation can lead to the decrease of a pong counter. If the counter reach the minimum than the termination
+     * process is started.
      * <br>
      * Invoking this method can lead, depending on the current state of the ClientNode, to verify if the connection to
      * the server is working through the emission of a ping to the server
