b33cd1e08da53bfd93dad22b0d70fda6e8859d4c
MarBerSpace <77501212+MarBerSpace@users.noreply.github.com>
2024-06-26 12:38:44 +0200

    Added and Implemented cancel (@override) method in ClientPingTask class

--- src/main/java/it/polimi/ingsw/am32/network/ClientNode/ClientPingTask.java
+++ src/main/java/it/polimi/ingsw/am32/network/ClientNode/ClientPingTask.java
@@ -3,9 +3,19 @@ package it.polimi.ingsw.am32.network.ClientNode;
 import java.util.TimerTask;
 
 public class ClientPingTask extends TimerTask {
-    private final ClientNodeInterface node;
+
+    private ClientNodeInterface node;
+
     public ClientPingTask(ClientNodeInterface node) {this.node = node;}
+
     public void run() {
         node.pongTimeOverdue();
+    }
+
+    @Override
+    public boolean cancel() {
+        boolean cancelled = super.cancel();
+        node = null;
+        return cancelled;
     }
 }
--- src/main/java/it/polimi/ingsw/am32/network/ServerNode/ServerPingTask.java
+++ src/main/java/it/polimi/ingsw/am32/network/ServerNode/ServerPingTask.java
@@ -6,11 +6,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.TimerTask;
 
 public class ServerPingTask extends TimerTask {
+
     private static final Logger logger = LogManager.getLogger(ServerPingTask.class);
+
     private NodeInterface node;
+
     public ServerPingTask(NodeInterface node) {
         this.node = node;
     }
+
     public void run() {
         // logger.debug("ServerPingTask started. The node {} will be checked for ping time.", node);  // Logging disabled to avoid spamming
         node.pingTimeOverdue();
@@ -22,5 +26,4 @@ public class ServerPingTask extends TimerTask {
         node = null;
         return cancelled;
     }
-
 }
